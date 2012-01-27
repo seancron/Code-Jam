85a1bdbca4beff2a19ce9c91b7e70693dd9edeb4
Sean Cronin <seancron@gmail.com>
2012-01-27 17:42:00 -0500

    Uses command line args for input filename now.
    Also renames the input/output files.

--- reversewords/reversewords.py
+++ reversewords/reversewords.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 
-def next_case():
+import sys
+
+def next_case(file_name):
     """A generator that returns the next case to be reversed"""
 
-    f = open('input.txt', 'r')
+    f = open(file_name, 'r')
     num_of_cases = int(f.readline())
 
     for i in range(num_of_cases):
@@ -11,7 +13,7 @@ def next_case():
 
 if __name__ == '__main__':
     i = 1
-    for sentence in next_case():
+    for sentence in next_case(sys.argv[1]):
         words = sentence.split()
         print "Case #" + str(i) + ':',
         print " ".join(reversed(words))
